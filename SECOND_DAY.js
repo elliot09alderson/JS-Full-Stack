c35eea6d24c63b7f058acac521d0882f817f2ef0
elliot09alderson <50716820+elliot09alderson@users.noreply.github.com>
2023-08-20 00:57:28 +0530

    Create SECOND_DAY.js

---
+++ SECOND_DAY.js
@@ -0,0 +1,56 @@
+// VARIBALES
+
+// let, var, const
+// const ui = 89; //constant
+// let apple = "friut"; //using let we can't re initalize variable
+
+// Functions
+
+// traditional function, Arrow Function, Call Back, IIFE, Object Functions
+
+// traditional function
+
+// Syntax: function __Function_name(__parameter) {
+
+//   __Function_Body;
+
+//   return __returnable_value
+// }
+
+// Example ->
+
+// var apple = "fruit";
+
+// function OddEven(num) {
+//   const result = num % 2 === 0 ? "even" : "odd";
+//   return result;
+// }
+
+// OddEven(14);
+// console.log(OddEven(16));
+// ____________________________________________________________________
+
+// Call Back Function   ()=>
+
+// Syntax: (__parameter) => __Return_value;
+
+// Example:
+// OBJECT
+// const fruit = {
+//   apple: "keeps doctor away ",
+//   pinapple: " helps to gain fibre",
+//   grapes: " sour in taste",
+//   calcFruit: (apple, pinapple) => apple + pinapple,
+// };
+// Syntax: __object_Name.__KEY_name
+// console.log(fruit.grapes);
+// console.log(fruit.calcFruit(45, 43));
+
+// ____________________________________________________
+
+// ARROW functions
+// Syntax:
+// const __Variable_name =(__params)=>__Return_value
+
+// const people = (a, b) => a+b
+// console.log(people(45, 43));
